a591ae8a090c734cb4fd028a7ac8d662f953dc21
DrakeFru <dfruhauf23@sandomenico.org>
2023-01-12 14:21:23 -0800

    5.3.2 submit

--- Card.java
+++ Card.java
@@ -1,14 +1,78 @@
-import greenfoot.*; 
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Game Board for Triples
+ * Write a description of class Card here.
  * 
  * @author (your name) 
  * @version (a version number or a date)
  */
-
 public class Card extends Actor
 {
-   
+    public enum Shape
+    {
+        TRIANGLE, RECTANGLE, CIRCLE, NO_SHAPE
+    }
+    
+    public enum Color
+    {
+        RED, GREEN, BLUE, NO_COLOR
+    }
+    
+    private Shape shape;
+    private Color color;
+    private boolean isSelected;
+    private GreenfootImage cardImage, selectedCardImage;
+    private int numberOfShapes, shading;
+    
+    public Card(Shape shape, Color color, int numberOfShapes, int shading, GreenfootImage cardImage, 
+                GreenfootImage selectedCardImage)
+    {
+        this.shape = shape;
+        this.color = color;
+        this.numberOfShapes = numberOfShapes;
+        this.shading = shading;
+        this.cardImage = cardImage;
+        this.selectedCardImage = selectedCardImage;
+        setImage(cardImage);
+    }
+    
+    public Shape getShape()
+    {
+        return shape;
+    }
+    
+    public Color getColor()
+    {
+        return color;
+    }
+    
+    public int getNumberOfShapes()
+    {
+        return numberOfShapes;
+    }
+    
+    public int getShading()
+    {
+        return shading;
+    }
+    
+    public GreenfootImage getCardImage()
+    {
+        return cardImage;
+    }
+    
+    public GreenfootImage getSelectedCardImage()
+    {
+        return selectedCardImage;
+    }
+    
+    public void setIsSelected(boolean isSelected)
+    {
+        this.isSelected = isSelected;
+    }
+    
+    public void act()
+    {
+        // Add your action code here.
+    }
 }
-
---
+++ Scorekeeper.java
@@ -0,0 +1,37 @@
+/**
+ * Write a description of class Scorekeeper here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Scorekeeper  
+{
+    private static int deckSize;
+    
+    private static int score;
+
+    private static long startTime = System.currentTimeMillis();
+
+    public static void setDeckSize(int size)
+    {
+        deckSize = size;
+    }
+    
+    public static void updateScore()
+    {
+        int timePassed = (int)((System.currentTimeMillis() - startTime) / 1000);
+        int points = deckSize - timePassed;  
+        if(points > 0)
+        {
+            score += points;
+        }
+        startTime = System.currentTimeMillis();
+    }
+    
+    public static int getScore()
+    {
+        return score;
+    }
+    
+    
+}
